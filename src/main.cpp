1eb66927f03f8f4ee92b1e490a47783337242cc3
CollumnThree <teresamnegea@proton.me>
2026-01-28 02:04:58 -0300

    Add handling for empty cd

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,9 @@
+#include "headers/KillProcess.hpp"
+#include "headers/builtin.hpp"
+#include "headers/utils.hpp"
 #include <algorithm>
 #include <cstdio>
+#include <cstdlib>
 #include <iostream>
 #include <map>
 #include <sched.h>
@@ -9,48 +13,55 @@
 #include <sys/wait.h>
 #include <unistd.h>
 #include <vector>
-#include "headers/utils.hpp"
-#include "headers/builtin.hpp"
-#include "headers/KillProcess.hpp"
+#include <format>
+
+
+const auto HomeDir = std::format("/home/{}", getenv("USER"));
+auto CurrentDir = PWDFunc();
 
 int main(int argc, char *argv[]) {
   std::string input;
-  auto CurrentDir = PWDFunc();
   while (true) {
     std::cout << CurrentDir << ">>> ";
     std::getline(std::cin, input);
     std::vector<std::string> full_command = split_args(input);
     std::vector<char *> command_char;
-    //Pointer pointing to KillProcess function(for later use in signal())
+    // Pointer pointing to KillProcess function(for later use in signal())
     void (*KPPointer)(int);
     KPPointer = &KillProcess;
-    //Check if the vector is empty
-    if(full_command.empty()){
-        continue;
+    // Check if the vector is empty
+    if (full_command.empty()) {
+      continue;
     }
-    //Check if the inserted command is a builtin function(e.g: cd)
+    // Check if the inserted command is a builtin function(e.g: cd)
     if (IsBCommand(full_command.at(0)) == true) {
-        auto& CommandList = BCommands;
-        switch (CommandList[full_command.at(0)]) {
-            case 1:
-                chdir(full_command.at(1).c_str());
-                CurrentDir = PWDFunc();
-                continue;
-            case 2:
-                std::cout << PWDFunc() << "\n";
-                continue;
-            case 3:
-                exit(0);
+      auto &CommandList = BCommands;
+      switch (CommandList[full_command.at(0)]) {
+      case 1:
+        if (full_command.size() < 2) {
+          CurrentDir = HomeDir;
+          chdir(HomeDir.c_str());
+          continue;
         }
+        chdir(full_command.at(1).c_str());
+        CurrentDir = PWDFunc();
+        continue;
+      case 2:
+        std::cout << PWDFunc() << "\n";
+        continue;
+      case 3:
+        exit(0);
+      }
     }
     // Converts full_command into a char * vector
     std::transform(full_command.begin(), full_command.end(),
                    std::back_inserter(command_char), convert);
     full_command.clear();
-    //Push command_char a nullptr to make it a null terminated string
+    // Push command_char a nullptr to make it a null terminated string(For later
+    // use in execvp)
     command_char.push_back(nullptr);
     pid_t command = fork();
-    //If fork() fails
+    // If fork() fails
     if (command < 0) {
       std::cout << "fork() fail \n";
       for (char *ptr : command_char) {
@@ -71,7 +82,7 @@ int main(int argc, char *argv[]) {
       signal(SIGINT, KPPointer);
       wait(NULL);
     }
-    //Deallocate all pointers
+    // Deallocate all pointers
     for (char *ptr : command_char) {
       delete[] ptr;
     }
